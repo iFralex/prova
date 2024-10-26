8a4df4561c787fae12f14ec0da450471cd15957a
iFralex <61825057+iFralex@users.noreply.github.com>
2024-10-26 02:03:29 +0200

    commit 10

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,7 @@
+import type { Metadata } from "next";
+
+import "./globals.css";
+import { Header } from "./components";
 
 export const metadata: Metadata = {
   title: "Next.js on Firebase App Hosting",
@@ -9,9 +13,18 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className="dark-theme">
+      <head>
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Google+Sans:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
-        <p>Ciao da layout</p>
+        <div className="dots" />
+        <Header />
         {children}
+        <div className="bottom-gradient" />
       </body>
     </html>
   );
